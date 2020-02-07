bb39518f93a916aaa6a7f00a5482b42f40e7a36e
Kazuto Ohara <kazuto.ohara@findy.co.jp>
2020-02-07 17:19:10 +0900

    Input Category Stats

--- lib/embulk/input/sendgrid.rb
+++ lib/embulk/input/sendgrid.rb
@@ -16,22 +16,23 @@ module Embulk
 
         columns = [
           Column.new(0, 'date', :timestamp),
-          Column.new(1, 'blocks', :long),
-          Column.new(2, 'bounce_drops', :long),
-          Column.new(3, 'bounces', :long),
-          Column.new(4, 'clicks', :long),
-          Column.new(5, 'deferred', :long),
-          Column.new(6, 'delivered', :long),
-          Column.new(7, 'invalid_emails', :long),
-          Column.new(8, 'opens', :long),
-          Column.new(9, 'processed', :long),
-          Column.new(10, 'requests', :long),
-          Column.new(11, 'spam_report_drops', :long),
-          Column.new(12, 'spam_reports', :long),
-          Column.new(13, 'unique_clicks', :long),
-          Column.new(14, 'unique_opens', :long),
-          Column.new(15, 'unsubscribe_drops', :long),
-          Column.new(16, 'unsubscribes', :long),
+          Column.new(1, 'category', :string),
+          Column.new(2, 'blocks', :long),
+          Column.new(3, 'bounce_drops', :long),
+          Column.new(4, 'bounces', :long),
+          Column.new(5, 'clicks', :long),
+          Column.new(6, 'deferred', :long),
+          Column.new(7, 'delivered', :long),
+          Column.new(8, 'invalid_emails', :long),
+          Column.new(9, 'opens', :long),
+          Column.new(10, 'processed', :long),
+          Column.new(11, 'requests', :long),
+          Column.new(12, 'spam_report_drops', :long),
+          Column.new(13, 'spam_reports', :long),
+          Column.new(14, 'unique_clicks', :long),
+          Column.new(15, 'unique_opens', :long),
+          Column.new(16, 'unsubscribe_drops', :long),
+          Column.new(17, 'unsubscribes', :long),
         ]
 
         resume(task, columns, 1, &control)
@@ -45,11 +46,20 @@ module Embulk
       end
 
       def run
-        json = RestClient.get("https://api.sendgrid.com/v3/stats?start_date=#{@task[:start_date]}&end_date=#{@task[:end_date]}", {:Authorization => "Bearer #{@task[:api_key]}", :content_type => 'application/json'})
-        results = JSON.parse(json)
+        max_category_limit = 100
 
-        results.each do |result|
-          @page_builder.add(result["stats"].first["metrics"].values.unshift(Time.parse(result["date"])))
+        date = Date.parse(@task[:start_date])
+        end_date = @task[:end_date] != "" ? Date.parse(@task[:end_date]) : Date.today
+
+        while date <= end_date do
+          json = RestClient.get("https://api.sendgrid.com/v3/categories/stats/sums?start_date=#{date.strftime("%Y-%m-%d")}&limit=#{max_category_limit}", {:Authorization => "Bearer #{@task[:api_key]}", :content_type => 'application/json'})
+          results = JSON.parse(json)
+
+          results["stats"].each do |metrics|
+            @page_builder.add(metrics["metrics"].values.unshift(metrics["name"]).unshift(Time.parse(results["date"])))
+          end
+
+          date = date.next_day
         end
 
         @page_builder.finish
